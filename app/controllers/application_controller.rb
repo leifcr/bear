1b93fe357cae3af26378d29ba8623aad1677529c
Matt West <matt.west@developercity.co.uk>
2012-05-18 10:36:11 +0100

    Enabled auth again

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-	#before_filter :authenticate
+	before_filter :authenticate
 
   self.append_view_path("lib/big_tuna/hooks")
   protect_from_forgery
