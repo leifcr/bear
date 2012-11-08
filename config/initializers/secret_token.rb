1ac148b39b2ef5c15ef6449e70b0e28966859f0c
Leif Ringstad <leifcr@gmail.com>
2012-11-08 14:32:49 +0100

    Change secret token

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-BigTuna::Application.config.secret_token = 'd5c574b218059ac8a655ae1a92657293edeb4ec44ed6b151c76be616c8eaec8b22add739da30b1e726c07e861d8022684ee3142491f9089675882a599d10411e'
+BigTuna::Application.config.secret_token = '47328940723097cba809cb3324c23bc5a65c43b5467a654c4b567a654cb56a745b8573290875923875980237522abc789b0cab7890e78907cba80789f7890bea0987c0987ba007342'
