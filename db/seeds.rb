fdad4b66f6b129bc17a5f40cff7e73f2b45cbd70
Leif Ringstad <leifcr@gmail.com>
2013-01-10 17:04:08 +0100

    Typo

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,5 @@
 u = User.new
-u.name = "Super Admin (Delete Me!"
+u.name = "Super Admin (Delete Me!)"
 u.email = "super_admin@local.host"
 u.password = "12345678"
 u.password_confirmation = "12345678"
